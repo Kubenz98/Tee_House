1485209bc744f03df8fd38730689aee4cca85a01
Jakub Kołuda <jko01@outlook.com>
2023-03-02 18:58:39 +0100

    Added ISR on demand

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,7 +1,12 @@
 import { NextApiHandler } from "next";
 
-const handleRevalidate: NextApiHandler = (req, res) => {
-  console.log("/api/revalidate received:", req.body);
+const handleRevalidate: NextApiHandler = async (req, res) => {
+  const event = req.body;
+  if (event.model === "product") {
+    const id = event.entry.id;
+    await Promise.all([res.revalidate("/"), res.revalidate(`/products/${id}`)]);
+    console.log(`revalidated product: ${id}`)
+  }
   res.status(204).end();
 };
 
