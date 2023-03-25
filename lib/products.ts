9fe46c0367af9f3ceed2aef359b569e2c2226521
Jakub Kołuda <jko01@outlook.com>
2023-03-25 14:21:09 +0100

    Changed image path.

--- lib/products.ts
+++ lib/products.ts
@@ -27,6 +27,6 @@ const transformProduct = (product: any): Product => {
     title: product.attributes.name,
     description: product.attributes.description,
     price: `$${product.attributes.price}`,
-    imageURL: `${CMS_URL}${product.attributes.image.data.attributes.url}`,
+    imageURL: product.attributes.image.data.attributes.url,
   };
 };
